e6a8b51830b653af2400fcbaba60cae7676fdac0
Mohit Saud <mohit@prabidhilabs.com>
2024-03-27 05:43:17 +0545

    Update Services sub-menu styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,6 @@
-// components/Header.js
-
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaChevronDown } from 'react-icons/fa';
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -19,6 +17,12 @@ const navigation = [
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+  const toggleMobileMenu = () => {
+    console.log('clicked');
+
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <header className="navbar navbar-expand-lg navbar-light bg-light header-section">
       <div className="container-fluid">
@@ -28,11 +32,10 @@ const Header = () => {
         >
           Call us at xxxxxx
         </Link>
-
         <button
           className="navbar-toggler"
           type="button"
-          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          onClick={toggleMobileMenu}
         >
           <FaBars className="outline-none" />
         </button>
@@ -42,17 +45,33 @@ const Header = () => {
           <ul className="navbar-nav mb-2 ms-auto mb-lg-0 justify-content-end lg:me-auto">
             {navigation.map((item, index) => (
               <li key={index} className="nav-item">
-                <Link href={item.href} className="nav-link px-4 fs-4">
-                  {item.name}
-                </Link>
+                {item.name === 'Services' ? (
+                  <div className="nav-link px-4 fs-4 services-link">
+                    {item.name} <FaChevronDown className="chevron-icon" />
+                    <div className="sub-menus">
+                      {/* Content of your sub-menus here */}
+                      <h5 className="text-green-600 p-2">
+                        Support Independent Living
+                      </h5>
+                      <h5 className="text-green-600 p-2">
+                        NDIS Support Coordination
+                      </h5>
+                      <h5 className="text-green-600 p-2">Individual Support</h5>
+                      <h5 className="text-green-600 p-2">
+                        Group Programs & Activities
+                      </h5>
+                    </div>
+                  </div>
+                ) : (
+                  <Link href={item.href} className="nav-link px-4 fs-4">
+                    {item.name}
+                  </Link>
+                )}
               </li>
             ))}
           </ul>
         </div>
       </div>
-      {mobileMenuOpen && (
-        <div className="overlay" onClick={() => setMobileMenuOpen(false)}></div>
-      )}
     </header>
   );
 };
