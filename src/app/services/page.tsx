140d0d5536fd3412799ce334d6495375fa18e2d8
Mohit Saud <mohit@prabidhilabs.com>
2024-03-29 10:06:53 +0545

    Add Services Page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Header/Header';
+import NDISProviderInfo from '@/components/NDISProviderInfo/NDISProviderInfo';
+
+const ServicePage = () => {
+  return (
+    <div>
+      <Header />
+      <NDISProviderInfo />
+    </div>
+  );
+};
+
+export default ServicePage;
