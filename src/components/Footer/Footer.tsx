3138b864363f22a8c071856ea0127004e151fa64
Mohit Saud <mohit@prabidhilabs.com>
2024-03-29 09:59:59 +0545

    Add logo and update footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,40 +1,62 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-4">
+    <footer className="bg-dark text-light py-4 mx-auto">
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
-            <h5>Contact Details</h5>
+          <div className="col-md-4 mb-4">
+            <Link href="/" className="navbar-brand">
+              <Image
+                width="150"
+                height="150"
+                src="/assets/logo-removebg.png"
+                alt="logo"
+                sizes="(max-width: 430px) 50vw, 430px"
+              />
+            </Link>
+          </div>
+          <div className="col-md-4 mb-4">
             <ul className="list-unstyled">
+              <h5>Contact Details</h5>
               <li>Address: 123 Main St, City, Country</li>
               <li>Phone: +1234567890</li>
               <li>Email: example@example.com</li>
             </ul>
           </div>
-          <div className="col-md-6 quick-links">
-            <h5>Quick Links</h5>
+          <div className="col-md-4 quick-links mb-4">
             <ul className="list-unstyled">
+              <h5>Quick Links</h5>
               <li>
-                <a href="#" className="text-decoration-none text-white">
+                <Link href="/" className="text-decoration-none text-white">
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-decoration-none text-white">
+                <Link
+                  href="/about-us"
+                  className="text-decoration-none text-white"
+                >
                   About Us
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-decoration-none text-white">
+                <Link
+                  href="/services"
+                  className="text-decoration-none text-white"
+                >
                   Services
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-decoration-none text-white">
+                <Link
+                  href="/contact-us"
+                  className="text-decoration-none text-white"
+                >
                   Contact Us
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
