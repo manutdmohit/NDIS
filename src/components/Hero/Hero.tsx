c93007baf7b3881087484261345b6f9a5abf8249
Mohit Saud <mohit@prabidhilabs.com>
2024-04-28 09:42:02 +0545

    Update Ability to Disability

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,11 +8,11 @@ const HeroSection = () => {
         <div className="row">
           <div className="col-md-12  intro">
             <p className="p-3 fs-4 text-white">
-              At Zestlife Ability, we are a spirited bunch of NDIS superheroes
-              on a mission to sprinkle some magic into everyday life. Our team
-              of expert wizards is here to offer a helping hand with personal
-              care and more because we believe in transforming challenges into
-              triumphs!{' '}
+              At Zestlife Disability, we are a spirited bunch of NDIS
+              superheroes on a mission to sprinkle some magic into everyday
+              life. Our team of expert wizards is here to offer a helping hand
+              with personal care and more because we believe in transforming
+              challenges into triumphs!{' '}
               <Link href="/about-us">
                 <button className="btn btn-primary fs-4">Read More</button>
               </Link>
