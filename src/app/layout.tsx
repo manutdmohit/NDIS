2d08776fc5521385330d42829ed217acac4e5d57
Mohit Saud <mohit@prabidhilabs.com>
2024-04-28 09:41:47 +0545

    Update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,11 @@ import Footer from '@/components/Footer/Footer';
 const barlow = Questrial({ weight: '400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'ZESTLIFE ABILITY',
+  title: 'ZESTLIFE | Thrive Behind Barriers',
   description:
-    'Thrive Behind Barriers | Helping Hands. At Zestlife Ability, we are a spirited bunch of NDIS superheroes on a mission to sprinkle some magic into everyday life. Our team of expert wizards is here to offer a helping hand with personal care and more because we believe in transforming challenges into triumphs!',
+    'Thrive Behind Barriers | Helping Hands. At Zestlife Disability, we are a spirited bunch of NDIS superheroes on a mission to sprinkle some magic into everyday life. Our team of expert wizards is here to offer a helping hand with personal care and more because we believe in transforming challenges into triumphs!',
   keywords:
-    'Disability, Support, Togetherness, Assistance, Positive Mindset, Determination, Right Support',
+    'Disability, Support, Togetherness, Assistance, Positive Mindset, Determination, Right Support, NDIS',
 };
 
 export default function RootLayout({
